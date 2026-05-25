ee13e19f92995d39aad6fe3a87a344999710808c
Shlok kumar <shlok200417@gmail.com>
2026-05-25 21:01:26 +0530

    Use extend method to add even numbers to list
    Replaced append with extend to add multiple values to the list.

--- sequence.py
+++ sequence.py
@@ -54,3 +54,8 @@ numbers = [1, 2, 3, 4, 5]
 even_numbers = [6, 8, 10]
 numbers.append(even_numbers)
 print(numbers) # [1, 2, 3, 4, 5, [6, 8, 10]]
+#extend,medthod - we extent the list with multiple value and append we extent the list with the  single method 
+numbers = [1, 2, 3, 4, 5]
+even_numbers = [6, 8, 10]
+numbers.extend(even_numbers)
+print(numbers)
