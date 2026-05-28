9893bfe620a37cc66cc867fce97ea03e5d843776
Shlok kumar <shlok200417@gmail.com>
2026-05-28 16:42:27 +0530

    Add indentation explanation comment in loops.py
    Added a comment explaining the importance of indentation.

---
+++ loops.py
@@ -0,0 +1,9 @@
+programming_languages = ['Rust', 'Java', 'Python', 'C++']
+
+for language in programming_languages:
+    print(language) 
+  """
+ here  Without that indentation,
+you would get an IndentationError:
+
+"""
