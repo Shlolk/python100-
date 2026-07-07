e9be5cf46287f5778164e8bdf0434a935b925a0b
Shlok kumar <shlok200417@gmail.com>
2026-07-07 23:55:54 +0530

    Create reteun_type_annotation.py

---
+++ reteun_type_annotation.py
@@ -0,0 +1,7 @@
+#->it's tells the what type of value function returns 
+#in this function it's return integer type value 
+def add(a: int, b: int) -> int:
+    return a + b
+
+result = add(10, 20)
+print(result)
