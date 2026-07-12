0890ff1710a069d8c891a85f5e2bcad57c889fac
Shlok kumar <shlok200417@gmail.com>
2026-07-12 07:34:51 +0530

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -1,6 +1,8 @@
+#in this code we are seeing that how to numpy module in array 
 import numpy as np
-
 arr = np.array([10, 20, 30, 40, 50])
-
 print(arr)
 print(type(arr)) 
+#cretaing the 2d array with the help of numpy 
+arr2= np.array([[1,4,7],[2,3,5]])
+print(arr2)
